2a4ce16789ccb9fa3428cee2974ae5fa1a33a7a9
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-09-13 14:05:41 +0400

    backup

--- chatWPF/MainWindow.xaml.cs
+++ chatWPF/MainWindow.xaml.cs
@@ -8,16 +8,42 @@ namespace chatWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ChatBubbleSent ChatBubbleSent { get { return ChatBubbleSent; } }
         public MainWindow()
         {
             InitializeComponent();
-            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
+            PreviewKeyDown += HandleEsc;
         }
 
         private void HandleEsc(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Escape)
-                Close();
+            if (e.Key != Key.Escape) return;
+            Close();
+        }
+
+        private void UIElement_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (TextBox.Text.Length > 0)
+            {
+                ChatBubbleSent.TextBlockSent.Text = TextBox.Text;
+                ListBox.Items.Add(new ChatBubbleSent());
+            }
+        }
+
+        private void Send_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (TextBox.Text.Length > 0)
+            {
+                //ChatBubbleSent.TextBlockSent.Text = TextBox.Text;
+                ChatBubbleSent cbs = new ChatBubbleSent();
+                cbs.TextBlockSent.Text = TextBox.Text;
+                ListBox.Items.Add(cbs);
+            }
+        }
+
+        private void MouseLeft_OnDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
